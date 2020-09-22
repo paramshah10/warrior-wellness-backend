77f901323701760b238cbd4f81c6b38682ae97e7
Param Shah <paramshah09@gmail.com>
2020-09-23 03:38:26 +0530

    createNewAccountTemplate endpoint added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,40 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { user_info, charts } from "./newUserTemplate";
+const cors = require('cors')({origin: true});
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const createNewAccountTemplate = functions.https.onRequest((request, response) => {
+    cors(request, response, async () => {
+        const uid = request.body.uid;
+        
+        const user = await admin.auth().getUser(uid);
+        if (!user) {
+            response.status(400).send("You are not authorized to perform this action");
+            return
+        }
+
+        user_info.firstName = request.body.firstName;
+        user_info.lastName = request.body.lastName;
+        user_info.email = request.body.email;
+
+        Promise.all([
+            admin.firestore().collection('users').doc(`${uid}`).set(user_info),
+            Object.entries(charts).forEach((entry) => {
+                admin.firestore().collection('users').doc(`${uid}`).collection('charts').doc(entry[0]).set(entry[1]);
+            }),
+        ])
+        .then(() => {
+            response.status(200).send({ message: 'Success', code: 200 });
+        })
+        .catch(() => {
+            console.log('Could not create new template when account created');
+            throw new functions.https.HttpsError('unknown','Could not create new template when account created');
+        });
+    })
+})
+
---
+++ functions/src/newUserTemplate.ts
@@ -0,0 +1,75 @@
+export const user_info = {
+    "cometchat_username": "",
+    "firstName": "",
+    "lastName": "",
+    "cometchat_listener_id": "",
+    "email": ""
+}
+
+export const journal = {}
+
+export const stress_incidents = {}
+
+export const charts = {
+    "overalls": {
+        "sleep": {
+            "percent_change": 0,
+            "score": 0,
+            "value_change": "up"
+        },
+        "stress": {
+            "percent_change": 0,
+            "score": 0,
+            "value_change": "up"
+        },
+        "stress_events": {
+            "percent_change": 0,
+            "score": 0,
+            "value_change": "up"
+        },
+        "stress_management": {
+            "percent_change": 0,
+            "score": 0,
+            "value_change": "up"
+        }
+    },
+    "sleep": {
+        "month": {
+            "awake": [],
+            "deep": [],
+            "light": [],
+            "total": []
+        },
+        "week": {
+            "awake": [],
+            "deep": [],
+            "light": [],
+            "total": []
+        }
+    },
+    "stress_bar": {
+        "day": [],
+        "month": [],
+        "week": []
+    },
+    "stress_bar_score": {
+        "month": [],
+        "week": []
+    },
+    "stress_bar_type": {
+        "month": [],
+        "week": []
+    },
+    "stress_management": {
+        "month": [],
+        "week": []
+    },
+    "stress_pie_score": {
+        "month": [],
+        "week": []
+    },
+    "stress_pie_type": {
+        "month": [],
+        "week": []
+    }
+}
